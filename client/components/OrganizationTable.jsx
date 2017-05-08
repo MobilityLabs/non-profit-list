85435802a5fa487036610f780fa46f7ae74e5ae8
Rick Metzger <rickymetz@gmail.com>
2017-05-08 12:16:14 -0400

    Add formating for tax filing date

--- client/components/OrganizationTable.jsx
+++ client/components/OrganizationTable.jsx
@@ -1,74 +1,91 @@
-import React from 'react';
+// @flow
+import React, {Component} from 'react';
 import moment from 'moment';
+import numeral from 'numeral';
 import _ from 'lodash';
 
 import './OrganizationList.scss';
 
-const OrganizationTable = ({organizations}) => {
+import type {Organizations} from '../types';
 
-  const table = organizations.map((o) => {
-    const filingDate = o.tax_period ? o.tax_period.toString().match(/.{1,4}/g) : "not found";
-    const month = moment(filingDate[1]).format("MMM");
-    const year = filingDate[0];
-    return (
-      <div key={o.ein} className="card">
-        <div className="card-block list-card-container">
-          <div className="card-container-interaction">
-            <label className="form-check-label">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                id={"checkbox-" + o.ein}
-                value={"checked-" + o.ein}
-                aria-label="..."
-              />
-            </label>
-            <label className="form-check-label">
-              <input
-                className="form-check-input star"
-                type="checkbox"
-                id={"star-" + o.ein}
-                value={"starred-" + o.ein}
-                aria-label="..."
-              />
-            </label>
-          </div>
-          <div className="card-container-company">
-            <div className="company-meta">
-              <h4 className="card-title font_normal mt-0">{o.name}</h4>
-              <dl>
-                <dt className="font_micro">NTEE Code</dt>
-                <dd className="card-text">{o.ntee_cd + " - "}</dd>
-              </dl>
-              <dl className="mb-0">
-                <dt className="font_micro">Location</dt>
-                <dd className="card-text">{_.startCase(_.toLower(o.city)) + ", " + o.state}</dd>
-              </dl>
+export default class OrganizationTable extends Component {
+  props: {
+    organizations: Organizations,
+  }
+  formatDate = (date) => {
+    let dateString = "Not Listed";
+    if (date) {
+      const filingDate = date.toString().match(/.{1,4}/g);
+      const month = moment(filingDate[1]).format("MMM");
+      const year = filingDate[0];
+      return(
+        dateString = month + " " + year
+      );
+    }
+    return dateString;
+  };
+  render() {
+    const {organizations} = this.props;
+    const table = organizations.map((o) => {
+      const amountValidator = (number: number) => {return number ? numeral(number).format('$0,0') : "Not Listed";};
+      return (
+        <div key={o.ein} className="card">
+          <div className="card-block list-card-container">
+            <div className="card-container-interaction">
+              <label className="form-check-label">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id={"checkbox-" + o.ein}
+                  value={"checked-" + o.ein}
+                  aria-label="..."
+                />
+              </label>
+              <label className="form-check-label">
+                <input
+                  className="form-check-input star"
+                  type="checkbox"
+                  id={"star-" + o.ein}
+                  value={"starred-" + o.ein}
+                  aria-label="..."
+                />
+              </label>
             </div>
-            <div className="company-financials">
-              <dl>
-                <dt className="font_micro">Income</dt>
-                <dd className="card-text font_small">{"$" + o.revenue_amt}</dd>
-              </dl>
-              <dl>
-                <dt className="font_micro">Assets</dt>
-                <dd className="card-text font_small">{"$" + o.asset_amt}</dd>
-              </dl>
-              <dl className="mb-0">
-                <dt className="font_micro">Last Logged Tax Filing</dt>
-                <dd className="card-text font_small">{month + " " + year}</dd>
-              </dl>
+            <div className="card-container-company">
+              <div className="company-meta">
+                <h4 className="card-title font_normal mt-0">{o.name}</h4>
+                <dl>
+                  <dt className="font_micro">NTEE Code</dt>
+                  <dd className="card-text">{o.ntee_cd + " - "}</dd>
+                </dl>
+                <dl className="mb-0">
+                  <dt className="font_micro">Location</dt>
+                  <dd className="card-text">{_.startCase(_.toLower(o.city)) + ", " + o.state}</dd>
+                </dl>
+              </div>
+              <div className="company-financials">
+                <dl>
+                  <dt className="font_micro">Income</dt>
+                  <dd className="card-text font_small">{amountValidator(o.revenue_amt)}</dd>
+                </dl>
+                <dl>
+                  <dt className="font_micro">Assets</dt>
+                  <dd className="card-text font_small">{amountValidator(o.asset_amt)}</dd>
+                </dl>
+                <dl className="mb-0">
+                  <dt className="font_micro">Last Logged Tax Filing</dt>
+                  <dd className="card-text font_small">{this.formatDate(o.tax_period)}</dd>
+                </dl>
+              </div>
             </div>
           </div>
         </div>
+      );
+    });
+    return (
+      <div className="card-group-vertical">
+        {table}
       </div>
     );
-  });
-  return (
-    <div className="card-group-vertical">
-      {table}
-    </div>
-  );
+  }
 }
-
-export default OrganizationTable;
