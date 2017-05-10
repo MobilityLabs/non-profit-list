cade9d8f65a774db785d0167ef3ea4083615b31b
Sean Perkins <sean@mobility-labs.com>
2017-05-10 11:29:13 -0400

    Remove state code that is not present on irs

--- scripts/download_organizations.js
+++ scripts/download_organizations.js
@@ -1,18 +1,34 @@
+// @flow
 require('dotenv').config({silent: true});
 const _ = require('lodash');
 const fs = require('fs');
 const https = require('https');
-const tmpDir = process.env.TMP_DIR;
+const tmpDir = process.env.TMP_DIR || './tmp';
 
-console.log('Starting download to ' + tmpDir);
+console.log('Starting download to ' + tmpDir); // eslint-disable-line
 
 // This downloads all US organizations
-const stateCodes = ["al","ak","az","ar","ca","co","ct","de","dc","fl","ga","hi","id","il","in","ia","ks",
-                    "ky","la","me","mh","md","ma","mi","mn","ms","mo","mt","ne","nv","nh","nj","nm","ny","nc","nd",
-                    "oh","ok","or","pa","pr","ri","sc","sd","tn","tx","ut","vt","va","wa","wv","wi","wy"];
+const stateCodes = [
+  "al", "ak", "az", "ar", "ca", "co", "ct", "de", "dc", "fl", "ga", "hi", "id", "il", "in", "ia",
+  "ks", "ky", "la", "me", "md", "ma", "mi", "mn", "ms", "mo", "mt", "ne", "nv", "nh", "nj", "nm",
+  "ny", "nc", "nd", "oh", "ok", "or", "pa", "pr", "ri", "sc", "sd", "tn", "tx", "ut", "vt", "va",
+  "wa", "wv", "wi", "wy"];
 
-let missingFiles = [];
-let intervalId
+const missingFiles = [];
+let intervalId;
+let counter = 0;
+
+const downloadMissingFile = (fileNames) => {
+  if (fileNames[counter] === undefined) {clearInterval(intervalId); return;}
+  const url = 'https://www.irs.gov/pub/irs-soi/' + fileNames[counter];
+  const file = fs.createWriteStream(tmpDir + fileNames[counter]);
+  https.get(url, (response) => {
+    response.pipe(file);
+  });
+  console.log('Downloaded ' + fileNames[counter]); // eslint-disable-line
+  counter++;
+};
+
 fs.readdir(tmpDir, (err, files) => {
   stateCodes.forEach((code) => {
     const fileName = 'eo_' + code + '.csv';
@@ -20,21 +36,9 @@ fs.readdir(tmpDir, (err, files) => {
       return;
     }
     missingFiles.push(fileName);
-  })
-  intervalId = setInterval(downloadMissingFile, 3000, missingFiles);
-})
-
-let counter = 0;
-
-const downloadMissingFile = (fileNames) => {
-  if (fileNames[counter] === undefined){clearInterval(intervalId); return;}
-  const url = 'https://www.irs.gov/pub/irs-soi/' + fileNames[counter];
-  let file = fs.createWriteStream(tmpDir + fileNames[counter]);
-  https.get(url, (response) => {
-    response.pipe(file);
   });
-  console.log('Downloaded ' + fileNames[counter]);
-  counter++;
-}
+  intervalId = setInterval(downloadMissingFile, 3000, missingFiles);
+});
 
-console.log('Done downloading to ' + tmpDir);
+
+console.log('Done downloading to ' + tmpDir); // eslint-disable-line
