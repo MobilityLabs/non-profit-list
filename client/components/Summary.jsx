af156336da0f7137e349a08dafd05360b12fb4d5
Rick Metzger <rickymetz@gmail.com>
2017-05-12 15:55:15 -0400

    create and style cummary component, add to dashboard container

---
+++ client/components/Summary.jsx
@@ -0,0 +1,117 @@
+// @flow
+import numeral from 'numeral';
+import React, {Component} from 'react';
+
+import './Summary.scss';
+
+export default class Summary extends Component {
+  props: {
+    summaryData: SummaryData,
+  }
+
+  render() {
+    const commas = (number: ?number) => {return number ? numeral(number).format('0,0') : "Not Available";};
+    const amountValidator = (number: ?number) => {return number ? numeral(number).format('$0,0.00') : "Not Available";};
+    const {summaryData} = this.props;
+    return (
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="summary">
+            <div className="summary-header">
+              <dl>
+                <dt className="font_micro">Complied Data across:</dt>
+                <dd className="h4">
+                  {commas(summaryData.count)} search results
+                  <small className="font_micro">{commas(summaryData.count)} search results of 1.6m Non-profits</small>
+                </dd>
+              </dl>
+            </div>
+            <div className="summary-meta">
+              <div className="summary-income">
+                <h4 className="label-heading h6">Income</h4>
+                <dl>
+                  <dt className="font_micro">Average</dt>
+                  <dd className="font_micro">
+                    {amountValidator(summaryData.income_avg)}
+                  </dd>
+                </dl>
+                <dl>
+                  <dt className="font_micro">Largest</dt>
+                  <dd className="font_micro">
+                    {amountValidator(summaryData.income_max)}
+                  </dd>
+                </dl>
+                <dl>
+                  <dt className="font_micro">Median</dt>
+                  <dd className="font_micro">
+                    {amountValidator(summaryData.income_med)}
+                  </dd>
+                </dl>
+                <dl>
+                  <dt className="font_micro">Smallest</dt>
+                  <dd className="font_micro">
+                    {amountValidator(summaryData.income_min)}
+                  </dd>
+                </dl>
+              </div>
+              <div className="summary-revenue">
+                <h4 className="label-heading h6">Revenue</h4>
+                <dl>
+                  <dt className="font_micro">Average</dt>
+                  <dd className="font_micro">
+                    {amountValidator(summaryData.revenue_avg)}
+                  </dd>
+                </dl>
+                <dl>
+                  <dt className="font_micro">Largest</dt>
+                  <dd className="font_micro">
+                    {amountValidator(summaryData.revenue_max)}
+                  </dd>
+                </dl>
+                <dl>
+                  <dt className="font_micro">Median</dt>
+                  <dd className="font_micro">
+                    {amountValidator(summaryData.revenue_med)}
+                  </dd>
+                </dl>
+                <dl>
+                  <dt className="font_micro">Smallest</dt>
+                  <dd className="font_micro">
+                    {amountValidator(summaryData.revenue_min)}
+                  </dd>
+                </dl>
+              </div>
+              <div className="summary-assets">
+                <h4 className="label-heading h6">Assets</h4>
+                <dl>
+                  <dt className="font_micro">Average</dt>
+                  <dd className="font_micro">
+                    {amountValidator(summaryData.asset_avg)}
+                  </dd>
+                </dl>
+                <dl>
+                  <dt className="font_micro">Largest</dt>
+                  <dd className="font_micro">
+                    {amountValidator(summaryData.asset_max)}
+                  </dd>
+                </dl>
+                <dl>
+                  <dt className="font_micro">Median</dt>
+                  <dd className="font_micro">
+                    {amountValidator(summaryData.asset_med)}
+                  </dd>
+                </dl>
+                <dl>
+                  <dt className="font_micro">Smallest</dt>
+                  <dd className="font_micro">
+                    {amountValidator(summaryData.asset_min)}
+                  </dd>
+                </dl>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- client/containers/DashboardPage.jsx
+++ client/containers/DashboardPage.jsx
@@ -12,6 +12,7 @@ import OrganizationList from '../components/OrganizationList';
 import SelectedPopover from '../components/SelectedPopover';
 import SortBar from '../components/SortBar';
 import StateFilter from '../components/StateFilter';
+import Summary from '../components/Summary';
 import {defaultFilters} from '../../filtersData';
 
 import type {Organizations, SummaryData, FiltersData, Filters} from '../types';
@@ -190,6 +191,11 @@ export default class DashboardPage extends Component {
           </div>
         </div>
         <div className="container py-3">
+          <div className="row">
+           <div className="col-sm-12">
+            <Summary summaryData={summaryData} />
+           </div>
+         </div>
           <div className="row">
             <div className="col-sm-12 col-md-4">
               <h3 className="h6 label-heading d-flex justify-content-between align-items-baseline">
