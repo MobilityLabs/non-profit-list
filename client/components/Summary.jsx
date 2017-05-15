4583ef245d69ee7d9d08db705d305aabf57d36c7
Rick Metzger <rickymetz@gmail.com>
2017-05-15 15:06:55 -0400

    Add css sticky position to summary component, simplify content

--- client/components/Summary.jsx
+++ client/components/Summary.jsx
@@ -15,54 +15,43 @@ export default class Summary extends Component {
     const amountValidator = (number: ?number) => {return number ? numeral(number).format('$0,0.00') : "Not Available";};
     const {summaryData, classes} = this.props;
     return (
-      <div className={"row " + classes}>
-        <div className="col-sm-12">
-          <div className="summary">
-            <div className="summary-header">
-              <dl>
-                <dt className="font_micro">Complied Data across:</dt>
-                <dd className="h4">
-                  {commas(summaryData.count)} search results
-                  <small className="font_micro">{commas(summaryData.count)} search results of 1.6m Non-profits</small>
-                </dd>
-              </dl>
-            </div>
-            <table className="table table-sm table-striped mb-0">
-              <thead>
-                <tr>
-                  <th/>
-                  <th className="font_small text-right">Average</th>
-                  <th className="font_small text-right">Largest</th>
-                  <th className="font_small text-right">Median</th>
-                  <th className="font_small text-right">Smallest</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row" className="label-heading h6">Income</th>
-                  <td className="font_micro text-right">{amountValidator(summaryData.income_avg)}</td>
-                  <td className="font_micro text-right">{amountValidator(summaryData.income_max)}</td>
-                  <td className="font_micro text-right">{amountValidator(summaryData.income_med)}</td>
-                  <td className="font_micro text-right">{amountValidator(summaryData.income_min)}</td>
-                </tr>
-                <tr>
-                  <th scope="row" className="label-heading h6">Revenue</th>
-                  <td className="font_micro text-right">{amountValidator(summaryData.revenue_avg)}</td>
-                  <td className="font_micro text-right">{amountValidator(summaryData.revenue_max)}</td>
-                  <td className="font_micro text-right">{amountValidator(summaryData.revenue_med)}</td>
-                  <td className="font_micro text-right">{amountValidator(summaryData.revenue_min)}</td>
-                </tr>
-                <tr>
-                  <th scope="row" className="label-heading h6">Assets</th>
-                  <td className="font_micro text-right">{amountValidator(summaryData.asset_avg)}</td>
-                  <td className="font_micro text-right">{amountValidator(summaryData.asset_max)}</td>
-                  <td className="font_micro text-right">{amountValidator(summaryData.asset_med)}</td>
-                  <td className="font_micro text-right">{amountValidator(summaryData.asset_min)}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+      <div className={"summary sticky-top " + classes}>
+        <table className="table table-sm table-striped mb-0">
+          <thead>
+            <tr>
+              <th className="font_small text-center table-name">
+                Summary of {commas(summaryData.count)} Returned Records
+              </th>
+              <th className="font_small text-right">Average</th>
+              <th className="font_small text-right">Largest</th>
+              <th className="font_small text-right">Median</th>
+              <th className="font_small text-right">Smallest</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row" className="label-heading h6">Income</th>
+              <td className="font_micro text-right">{amountValidator(summaryData.income_avg)}</td>
+              <td className="font_micro text-right">{amountValidator(summaryData.income_max)}</td>
+              <td className="font_micro text-right">{amountValidator(summaryData.income_med)}</td>
+              <td className="font_micro text-right">{amountValidator(summaryData.income_min)}</td>
+            </tr>
+            <tr>
+              <th scope="row" className="label-heading h6">Revenue</th>
+              <td className="font_micro text-right">{amountValidator(summaryData.revenue_avg)}</td>
+              <td className="font_micro text-right">{amountValidator(summaryData.revenue_max)}</td>
+              <td className="font_micro text-right">{amountValidator(summaryData.revenue_med)}</td>
+              <td className="font_micro text-right">{amountValidator(summaryData.revenue_min)}</td>
+            </tr>
+            <tr>
+              <th scope="row" className="label-heading h6">Assets</th>
+              <td className="font_micro text-right">{amountValidator(summaryData.asset_avg)}</td>
+              <td className="font_micro text-right">{amountValidator(summaryData.asset_max)}</td>
+              <td className="font_micro text-right">{amountValidator(summaryData.asset_med)}</td>
+              <td className="font_micro text-right">{amountValidator(summaryData.asset_min)}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
